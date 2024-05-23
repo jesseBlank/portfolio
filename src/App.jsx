e59718bb608ac75ae79fe08812f7d3fe50dd8023
Jesse Blank <jesselloydblank@gmail.com>
2024-05-22 17:28:23 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename={"/porfolio"}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/main" element={<Main />} />
